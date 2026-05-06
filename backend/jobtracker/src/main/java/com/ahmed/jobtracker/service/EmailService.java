01a1da6bebf936a87d336f60c38f16887a3bee84
ahmeddarwish2793 <ahmed.darwish2793@gmail.com>
2026-05-06 22:26:38 +0200

    Switch to SendGrid email

--- backend/jobtracker/src/main/java/com/ahmed/jobtracker/service/EmailService.java
+++ backend/jobtracker/src/main/java/com/ahmed/jobtracker/service/EmailService.java
@@ -1,29 +1,47 @@
 package com.ahmed.jobtracker.service;
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-    private final JavaMailSender mailSender;
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+
+    @Value("${SENDGRID_API_KEY}")
+    private String sendGridApiKey;
 
     @Async
     public void sendPasswordResetEmail(String toEmail, String resetLink) {
-        SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setTo(toEmail);
-        message.setSubject("Password Reset Request");
-        message.setText(
+        Email from = new Email("jobtracker.app.student@gmail.com"); // your verified sender
+        String subject = "Password Reset Request";
+        Email to = new Email(toEmail);
+
+        Content content = new Content("text/plain",
                 "Click the link below to reset your password:\n\n" +
                         resetLink +
-                        "\n\nIf you did not request this, ignore this email."
-        );
+                        "\n\nIf you did not request this, ignore this email.");
 
-        mailSender.send(message);
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+
+            Response response = sg.api(request);
+
+            System.out.println("SendGrid Status: " + response.getStatusCode());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
